d3bc74c983ac4b19eb5a6e1bf017cc181b2a8a99
xjx199403 <79250307+xjx199403@users.noreply.github.com>
2021-06-07 09:34:48 +0800

    Update CityDataController.java

--- src/CityDataController.java
+++ src/CityDataController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+/**
+测试controller
+**/
 @RestController
 @RequestMapping("/api")
 public class CityDataController {
